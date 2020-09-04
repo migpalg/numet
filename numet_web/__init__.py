6e46ce940413b32738f22563a545b86dad50ecc7
Miguel Palacio <migpalg@outlook.es>
2020-09-04 10:13:09 -0500

    Added index page

--- numet_web/__init__.py
+++ numet_web/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+
+from flask import Flask, render_template, send_from_directory
+
 from . import single_variable
 
 
@@ -6,4 +9,14 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.register_blueprint(single_variable.bp)
+
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'),
+                                   'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
     return app
